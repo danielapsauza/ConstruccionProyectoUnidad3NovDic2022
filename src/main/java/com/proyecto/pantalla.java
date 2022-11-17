00a4ed848f108a9caee4c4a51a88c483f400cb18
danielapsauza <a20216406@alumnos.uady.mx>
2022-11-17 10:11:59 -0600

    primer commit

---
+++ src/main/java/com/proyecto/pantalla.java
@@ -0,0 +1,31 @@
+package com.proyecto;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class pantalla {
+
+    public static void main(String[] args) {
+
+        JFrame pantalla = new JFrame("Pantalla Dummie");
+
+        String[][] informacion = {
+                { "ID1", "firstName1", "lastName1", "photo1" },
+                { "ID2", "firstName2", "lastName2", "photo2" },
+                { "ID3", "firstName3", "lastName3", "photo3" }
+        };
+
+        String[] columnas = { "ID", "firstName", "lastName", "photo" };
+
+        JTable tablaInformacion = new JTable(informacion, columnas);
+
+        JScrollPane scrollPanel = new JScrollPane(tablaInformacion);
+
+        pantalla.add(scrollPanel);
+        pantalla.setSize(500, 150);
+        pantalla.setVisible(true);
+    }
+
+
+}
