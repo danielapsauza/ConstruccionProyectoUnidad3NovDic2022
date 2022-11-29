630ce87e0fd1ec97a066849adbefcb90794ec3c2
EmilianoValenciaGuzman <evalencia142000@gmail.com>
2022-11-28 20:05:55 -0600

    boton

--- src/main/java/com/proyecto/pantalla.java
+++ src/main/java/com/proyecto/pantalla.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -24,14 +25,16 @@ public class pantalla {
 
     public pantalla(List<ArrayList<Object>> informacionArray){
         crearPantalla(convertidorAMatriz(informacionArray));
-        System.out.println("si salió");
+        System.out.println("aaaa");
     }
 
     public void crearPantalla(Object[][] matrizEmpleados){
         JFrame pantalla = new JFrame("Pantalla Dummie");
-
         Object[][] informacion = matrizEmpleados;
         String[] columnas = { "ID", "firstName", "lastName", "photo" };
+
+        JButton button = new JButton("Modificar");
+        button.setBounds(450,700,95,30);
 
         DefaultTableModel model = new DefaultTableModel(informacion, columnas) {
             @Override
@@ -40,16 +43,32 @@ public class pantalla {
             }
         };
 
-        JTable tablaInformacion = new JTable(model);
-        JScrollPane scrollPanel = new JScrollPane(tablaInformacion);
+        JTable tablaInformacion = new JTable(model){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                if(column == 0 || column == 3){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        };
+        tablaInformacion.setBounds(500, 500, 700, 400);
+        tablaInformacion.setRowHeight(200);
 
+        JScrollPane scrollPanel = new JScrollPane(tablaInformacion);
+        scrollPanel.setBounds(500, 500, 700, 400);
+
+        pantalla.add(button);
         pantalla.add(scrollPanel);
-        pantalla.setSize(500, 150);
+        
+        pantalla.setSize(1000, 1000);
         pantalla.setVisible(true);
     }
 
     public Object[][] convertidorAMatriz(List<ArrayList<Object>> informacionArray){
         int i = 0;
+       
         Object[] info = informacionArray.toArray();
         Object[][] informacion = new Object[info.length][4];
 
@@ -60,13 +79,15 @@ public class pantalla {
             informacion[i][0] = aux2[0].toString();
             informacion[i][1] = aux2[1].toString();
             informacion[i][2] = aux2[2].toString();
+
             informacion[i][3] = getImgIcon(aux2[3].toString());
+            
             i++;
         }
 
         return informacion;
     }
-    
+
     private Icon getImgIcon(String urlStr) {
 
         try {
@@ -79,8 +100,12 @@ public class pantalla {
 
         return null;
     }
-
-    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException, ValidationException {
+    public static void main(String[] args) throws ValidationException, FileNotFoundException, IOException, ParseException {
+        JsonManager jsonManager = new JsonManager();
+        JSONArray jsonArray = jsonManager.readJson("src/employees.json");
+        jsonManager.jsonValidation(jsonArray, "employee"); 
+        pantalla pantalla = new pantalla(jsonManager.jsonConverterToObject(jsonArray));
     }
+
 
 }
