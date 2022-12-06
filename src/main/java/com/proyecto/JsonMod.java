0a121fb41d2259ba9d834e0bd52dbfc9d8614ed4
EmilianoValenciaGuzman <evalencia142000@gmail.com>
2022-12-05 19:42:02 -0600

    pantalla v5

--- src/main/java/com/proyecto/JsonMod.java
+++ src/main/java/com/proyecto/JsonMod.java
@@ -109,6 +109,48 @@ public class JsonMod extends JFrame implements ActionListener{
     }
 
     public void agregarEmpleadoPantalla(){
+        accion = "agregar";
+        setLayout(null);
+        setBounds(0,0,450,180);
+        setTitle("Modificar Empleado");
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        label1=new JLabel("Nombre:");
+        label1.setBounds(10,10,100,30);
+        add(label1);
+
+        label1=new JLabel("Apellido:");
+        label1.setBounds(10,40,100,30);
+        add(label1);
+
+        label1=new JLabel("Src Imagen:");
+        label1.setBounds(10,70,100,30);
+        add(label1);
+
+        textFieldName=new JTextField();
+        textFieldName.setBounds(120,10,200,20);
+        add(textFieldName);
+
+        textFieldLastName=new JTextField();
+        textFieldLastName.setBounds(120,40,200,20);
+        add(textFieldLastName);
+
+        textFieldImg=new JTextField();
+        textFieldImg.setBounds(120,70,300,20);
+        add(textFieldImg);
+
+        buttonAceptar.setBounds(10,100,100,30);
+        add(buttonAceptar);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
     }
 
     public JButton getButtonAceptar() {
